790923ca33de2db4c59b512bf33788d57ea8d295
JanCaesar <janCaesar@github.com>
2019-06-15 22:01:56 -0700

    Display information from DB

--- DjangoLogIn/logIn/MainApp/articlesApp/admin.py
+++ DjangoLogIn/logIn/MainApp/articlesApp/admin.py
@@ -6,3 +6,5 @@ admin.site.register(Article)
 
 # add - from .models import Article (. in .models is the saying the current dir)
 # admin.site.register(Article) - telling Django to register Article class(OBJECT) from models.py
+# now we can also use the data from DB and display to the front-end
+# render Article inside views.py
--- DjangoLogIn/logIn/MainApp/articlesApp/views.py
+++ DjangoLogIn/logIn/MainApp/articlesApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from .models import Article 
 
 # Create your views here.
 def articleList(request):
-    return render(request, 'articles/articleList.html') # articles/ is from the template inside articlesApp
+    articlesPutok = Article.objects.all().order_by('date')
+    return render(request, 'articles/articleList.html', {'articlesAnglit': articlesPutok}) # articles/ is from the template inside articlesApp
+
+
+# from .models import Article - to access the information inside tye DB (import . to import all class/object)
+# line 6 is getting ALL data inside the Articles table from the DB
+# , {'articlesAnglit': articlesPutok} - includes on rendering info to the front-end
+
+"""{% articlesAnglit %} - this is to use python code called template tag (equivalent of razer syntax in C#)
+{{ }} output information out of python code"""
